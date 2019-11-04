b64a5c8558675c79fcd2021c988b873d33f04b17
Rahulkumarraman <30779469+Rahulkumarraman@users.noreply.github.com>
2019-11-04 20:50:59 +0530

    Add files via upload
    Right view of Tree 
    Plz change class name to run program

---
+++ Right_view_of_Tree.java
@@ -0,0 +1,51 @@
+class Node
+{
+	int data;
+	Node left;
+	Node right;
+	
+}
+class BinaryTree
+{
+	public Node createNewNode(int k)
+	{
+		Node a=new Node();
+		a.data=k;
+		a.left=null;
+		a.right=null;
+		return a;
+	}
+	int maxlevel;
+	public void rightview(Node node,int level)
+	{
+		if(node==null)
+			return;
+		if(level>=maxlevel)
+		{
+			System.out.print(node.data+" ");
+			maxlevel++;
+			
+		}
+		rightview(node.right,level+1);
+		rightview(node.left,level+1);
+	}
+	
+	
+}
+public class BinaryTreeApp 
+{
+     public static void main(String[] args)
+     {
+    	 BinaryTree a=new BinaryTree();
+    	 Node root=a.createNewNode(2);
+    	 root.left=a.createNewNode(7);
+    	 root.right=a.createNewNode(5);
+    	 root.left.left=a.createNewNode(2);
+    	 root.left.right=a.createNewNode(6);
+    	 root.left.right.left=a.createNewNode(5);
+    	 root.left.right.right=a.createNewNode(11);
+    	 root.right.right=a.createNewNode(9);
+    	 root.right.right.left=a.createNewNode(4);
+   a.rightview(root,0);
+     }
+}
